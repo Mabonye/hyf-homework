bd81b8a5e533461d6f8401b471bcaec08e38a3ea
Jemima Masamu <mimamenser@yahoo.com>
2022-11-22 00:13:27 +0100

    Dog age calculator task implemented

---
+++ javascript/javascript1/week1/dog_age_calculator.js
@@ -0,0 +1,14 @@
+// Goodboy-Oldboy (A dog age calculator)
+
+// Variable declaration and assignment
+const dogYearOfBirth = 2020;
+const dogYearFuture = 2045;
+let dogYear = dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears = false;
+
+if (shouldShowResultInDogYears) {
+    console.log("Your dog will be " + dogYear * 10 + " dog years old in " + dogYearFuture);
+} else {
+    let dogYear = dogYearFuture - dogYearOfBirth;
+    console.log("Your dog will be " + dogYear + " human years old in " + dogYearFuture);
+}

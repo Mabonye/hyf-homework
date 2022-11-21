ab44aab480edba0d0dc8065857860fd6b9ac2b8c
Jemima Masamu <mimamenser@yahoo.com>
2022-11-22 00:11:52 +0100

    Age calculator task implemented

---
+++ javascript/javascript1/week1/age_calculator.js
@@ -0,0 +1,8 @@
+// Age-ify (A future age calculator)
+
+// Variable declaration and assignment
+const yearOfBirth = 1990;
+const yearFuture = 2040;
+let age = yearFuture - yearOfBirth;
+
+console.log("You will be " + age + " years old in " + yearFuture);

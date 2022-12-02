02240b99019317f028154df7de525e6f64ea141e
Jemima Masamu <mimamenser@yahoo.com>
2022-12-02 18:32:58 +0100

    Candy-helper file was modified after review

--- javascript/javascript1/week2/candy-helper.js
+++ javascript/javascript1/week2/candy-helper.js
@@ -1,26 +1,31 @@
 const boughtCandyPrices = [];
 const myObj = {
-    "sweet" : 0.5,
-    "chocolate" : 0.7,
-    "toffee" : 1.1,
-    "chewing-gum" : 0.03
+    "sweet": 0.5,
+    "chocolate": 0.7,
+    "toffee": 1.1,
+    "chewing-gum": 0.03
 };
 let amountToSpend = Math.random() * 100;
 
-function addCandy(candyType, weight){
-    boughtCandyPrices.push(myObj[candyType] * weight);
-    return boughtCandyPrices;
+// Function for adding candy prices into array
+function addCandy(candyType, weight) { 
+    if (myObj.hasOwnProperty(candyType)) {
+        boughtCandyPrices.push(myObj[candyType] * weight);
+        return boughtCandyPrices;
+    }
+    return "The candy type is not available";  
 }
 
-function canBuyMoreCandy(){
+
+function canBuyMoreCandy() {
     //console.log(boughtCandyPrices);
     let i = 0;
     let total = 0;
-    while (i < boughtCandyPrices.length){
-        total  += boughtCandyPrices[i];
+    while (i < boughtCandyPrices.length) {
+        total += boughtCandyPrices[i];
         i++;
     }
-
+    
     if (amountToSpend > total) {
         //console.log(amountToSpend, total);
         console.log("You can buy more, so please do!");
@@ -30,7 +35,7 @@ function canBuyMoreCandy(){
 
 }
 
-console.log(addCandy("sweet", 20));
+console.log(addCandy("mint", 20));
 console.log(addCandy("toffee", 20));
 console.log(addCandy("chewing-gum", 20));
 console.log(addCandy("chocolate", 20));

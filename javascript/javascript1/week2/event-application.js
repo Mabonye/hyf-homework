34817a278b9ff928ac0a387553229b4295c8d612
Jemima Masamu <mimamenser@yahoo.com>
2022-12-02 18:33:41 +0100

    Event-application file was modified after review

--- javascript/javascript1/week2/event-application.js
+++ javascript/javascript1/week2/event-application.js
@@ -1,22 +1,26 @@
-let days = 
-["Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday"]; 
+let days =
+    ["Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday"];
 const todayDate = new Date(); //Gets today's date
 
-function getEventWeekday(numberOfDays){
+function getEventWeekday(numberOfDays) {
     /*
     todayDate.getDay() gets the index for day of the week on todayDate
     (7 - index) returns number of days left in a week after todayDate
     */
 
-    if (numberOfDays > (7 - todayDate.getDay())){
-        let eventDayIndex =  (numberOfDays - (7 - todayDate.getDay())) % 7;
-        console.log("The event will be held on a " + days[eventDayIndex]); 
+    if (typeof numberOfDays === "number") {
+        if (numberOfDays > (7 - todayDate.getDay())) {
+            let eventDayIndex = (numberOfDays + todayDate.getDay()) % 7;
+            console.log("The event will be held on a " + days[eventDayIndex]);
+        } else {
+            let newIndex = todayDate.getDay() + numberOfDays;
+            console.log("The event will be held on a " + days[newIndex]);
+        }
     } else {
-        let newIndex = todayDate.getDay() + numberOfDays;
-        console.log("The event will be held on a " + days[newIndex]);
+        console.log("Please, enter the scheduled days for the event");
     }
 
 }
 
-getEventWeekday(6);
+getEventWeekday(4);
 
